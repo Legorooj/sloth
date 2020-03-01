d970a41b647e6a93e69db2785a84a9b90f486e39
Legorooj <legorooj@protonmail.com>
2020-03-01 17:05:41 +1100

    add generator method to data processing class and add docstrings to public functions

--- sloth/data/__init__.py
+++ sloth/data/__init__.py
@@ -10,7 +10,7 @@ import tqdm
 
 
 __all__ = [
-    'BYTE', 'KILOBYTE', 'MEGABYTE', 'GIGABYTE', 'TestDataProcessing'
+    'BYTE', 'KILOBYTE', 'MEGABYTE', 'GIGABYTE', 'TestIncrementalDataProcessing'
 ]
 
 
@@ -20,18 +20,38 @@ MEGABYTE = KILOBYTE * 1024
 GIGABYTE = MEGABYTE * 1024
 
 
-class TestDataProcessing:
+class TestIncrementalDataProcessing:
     """
     TestDataProcessing is a class that runs data
     through a function while timing it.
     """
     
     def __init__(self, func):
+        """
+        Constructor
+        :param func: function to speedtest
+        :type func: function
+        """
         self.func = func
         if not callable(func):
             raise TypeError('Uncallable function')
     
-    def run(self, against=None, data=KILOBYTE, average=False, atimes=2):
+    def run(self, against=None, data=KILOBYTE, average=False, atimes=2, generator=False):
+        """
+        Run the speedtest
+        
+        :param against: list of integers to run against
+        :type against: list[int]
+        :param data: data to run through the function
+        :type data: str
+        :param average: average the speedtest to get more accurate results
+        :type average: bool
+        :param atimes: number of times to average. Defaults to 2.
+        :type atimes: int
+        :param generator: Run as a generator. This is used for exceptionally large runs.
+        :type generator: bool
+        :return: list containing the length(s) of time that it took to run the function
+        """
         if against is None:
             against = [1]
         else:
@@ -42,6 +62,9 @@ class TestDataProcessing:
         
         check_type(bool, average=average)
         check_type(int, atimes=atimes)
+        
+        if generator:
+            return self._run_generator(against, data, average, atimes)
         
         times = []
         
@@ -56,7 +79,31 @@ class TestDataProcessing:
                 
         return times
     
+    def _run_generator(self, against, data, average, atimes):
+        for index in range(len(against)):
+            if average:
+                averagetimes = []
+                for i in range(atimes):
+                    averagetimes.append(self._run(against[index], data))
+                yield sum(averagetimes) / len(averagetimes)
+            else:
+                yield self._run(against[index], data)
+    
     def run_with_progressbar(self, against=None, data=KILOBYTE, average=False, atimes=2):
+        """
+        Run the speedtest with a command line progress bar
+
+        :param against: list of integers to run against
+        :type against: list[int]
+        :param data: data to run through the function
+        :type data: str
+        :param average: average the speedtest to get more accurate results
+        :type average: bool 
+        :param atimes: number of times to average. Defaults to 2.
+        :type atimes: int
+        :return: list containing the length(s) of time that it took to run the function
+        """
+        
         if against is None:
             against = [1]
     
