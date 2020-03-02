14c071c1553e2bba56b2c570fbb57908a9b0afe7
Legorooj <legorooj@protonmail.com>
2020-03-02 17:37:05 +1100

    Add type checking on Timer class

--- sloth/timers.py
+++ sloth/timers.py
@@ -6,8 +6,9 @@
 # ----------------------------------------------------------------------------
 
 import time
-from ._types import ZeroFloat
+from ._types import ZeroFloat, function_type, NoneType
 from threading import Thread, Event
+from ._utils import check_type
 
 __all__ = [
     'Stopwatch', 'Timer'
@@ -16,32 +17,52 @@ __all__ = [
 
 class Stopwatch:
     """
-    Base stopwatch class for timing.
-    There may be slight overhead on this class of ~1 microseconds (~0.0001s)
+    Simple stopwatch for capturing code execution time.
     """
     
     def __init__(self):
         self._time = 0
     
     def start(self):
+        """
+        
+        Starts the *StopWatch*.
+        """
         self._time = time.time()
     
     def stop(self):
         """
-        Get the amount of time since start was called, and reset the timer.
-        :return: The amount of time since `start` was called.
+        Clears the stopwatch and returns the time elapsed since the :py:meth:`start` method has been called.
+        This method will return ``0`` if start has not been called.
+
+        :returns: The time - in seconds - elapsed since :py:meth:`start` was called
+        :rtype: float
         """
         t = ZeroFloat(time.time()) - self._time
         self._time = 0
         return t
     
     def lap(self):
+        """
+        Returns the time elapsed since the :py:meth:`start` method was called *without* clearing the stopwatch.
+        This method will return ``0`` if start has not been called.
+
+        :returns: The time - in seconds - elapsed since :py:meth:`start` was called
+        :rtype: int
+        """
         return ZeroFloat(time.time()) - self._time
 
 
 class Timer(Thread):
     
     def __init__(self, seconds, func, args=None, kwargs=None):
+        check_type(int, seconds=seconds)
+        check_type(function_type, func=func)
+        if not isinstance(args, NoneType):
+            check_type((list, tuple), args=args)
+        if not isinstance(kwargs, NoneType):
+            check_type(dict, kwargs=kwargs)
+        
         super(Timer, self).__init__(daemon=True)
         self._seconds = seconds
         self._func = func
