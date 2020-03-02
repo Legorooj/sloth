444c92ac232e93799c40d4d1ac284ae4927d4fd0
Legorooj <legorooj@protonmail.com>
2020-03-02 18:36:57 +1100

    Change pygments theme in documentation to tango

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,6 +48,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+pygments_style = 'tango'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
