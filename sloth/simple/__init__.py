dad7ad92ccad0e998331c713d19c00d18e0ef734
Legorooj <legorooj@protonmail.com>
2020-03-05 20:47:32 +1100

    Add time_func and time_code snippets

--- sloth/simple/__init__.py
+++ sloth/simple/__init__.py
@@ -5,12 +5,36 @@
 # Please see the LICENSE file in the root of this repository for more details.
 # ----------------------------------------------------------------------------
 
-from sloth.timers import Timer
+from sloth.timers import Timer, Stopwatch
+from sloth._utils import check_type
 
 __all__ = [
-    'call_after'
+    'call_after', 'time_func', 'time_code'
 ]
 
 
 def call_after(seconds, func, args=None, kwargs=None):
     Timer(seconds, func, args, kwargs).start()
+    
+    
+def time_func(func, iterations=1000):
+    check_type(int, iterations=iterations)
+    times = []
+    for i in range(iterations):
+        s = Stopwatch()
+        s.start()
+        func()
+        times.append(s.stop())
+    return sum(times) / len(times)
+
+
+def time_code(snippet, iterations=1000, gs=None, lcs=None):
+    check_type(int, iterations=iterations)
+    check_type(str, snippet=snippet)
+    times = []
+    for i in range(iterations):
+        s = Stopwatch()
+        s.start()
+        eval(snippet, gs, lcs)
+        times.append(s.stop())
+    return sum(times) / len(times)
