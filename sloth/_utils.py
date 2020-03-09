f499816c1a83d94c6c634c96507961eebd459b1c
Legorooj <legorooj@protonmail.com>
2020-03-09 22:14:50 +1100

    Update utils with more accurate iterable checking and subclass checking function

--- sloth/_utils.py
+++ sloth/_utils.py
@@ -6,20 +6,28 @@
 # ----------------------------------------------------------------------------
 
 __all__ = [
-    'check_type', 'is_iterable', 'is_function_or_callable'
+    'check_type', 'is_iterable', 'is_function_or_callable', 'check_subclass'
 ]
 
 
 def check_type(_type, **kwargs):
     for key, val in kwargs.items():
         if not isinstance(val, _type):
-            raise TypeError('{} must be {}'.format(key, _type))
+            raise TypeError('{} must be {}'.format(key, getattr(_type, '_type_str', _type)))
         
 
-def is_iterable(f):
-    if hasattr(f, '__iter__'):
+def check_subclass(klass, **kwargs):
+    for key, val in kwargs.items():
+        if not issubclass(val, klass):
+            raise TypeError('{} must be {}'.format(key, getattr(klass, '_type_str', klass)))
+        
+
+def is_iterable(obj):
+    try:
+        iter(obj)
         return True
-    return False
+    except TypeError:
+        return False
 
 
 def is_function_or_callable(f):
